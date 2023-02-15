b3720c9e45382808e3f2ea01dc111617a97db8da
Dmytro Kharchenko <dmytro190589@gmail.com>
2023-02-15 16:24:26 +0200

    header

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 // import { Navigation } from '../Navigation/Navigation';
 import { Outlet } from 'react-router';
-import Header from 'components/Header/Header';
 import { DashBoard } from 'components/DashBoard/DashBoard';
+import Header from 'components/Header/Header';
 // import { Login } from 'pages/LoginPage/LoginPage';
 // import { Registration } from 'pages/RegistrationsPage/RegistrationsPage';
 
