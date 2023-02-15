e4097ddc7b6c6f36f713444429e211bf8d3c72e0
Dmytro Kharchenko <dmytro190589@gmail.com>
2023-02-15 16:42:18 +0200

    123

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -1,9 +1,5 @@
 import { Navigation } from '../Navigation/Navigation';
-<<<<<<< HEAD
-// import { Outlet } from 'react-router';
-=======
 
->>>>>>> main
 import { Balance } from 'components/Balance/Balance';
 import { Currency } from 'components/Currency/Currency';
 
