0317134af2dd63ba75738d1437d85d7ba5aa668c
nataliapovaliy <natalia.bg1125@gmail.com>
2023-02-15 22:07:22 +0200

    statistics

---
+++ src/redux/statistics/stat-operations.js
@@ -0,0 +1,15 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const statistSummary = createAsyncThunk (
+    'statistics',
+    async (credentials, { rejectWithValue }) => {
+        try {
+            const { data } = await axios.get('/api/transactions-summary', credentials);
+            // token.set(data.token);
+            console.log('statistics >>>>', data);
+            return data;
+        } catch (error) {}
+    }
+)
+
