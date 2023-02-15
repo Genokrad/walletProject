f2a4eec9cb1661f53d67806a2030b5a02d2f018e
Dmytro Kharchenko <dmytro190589@gmail.com>
2023-02-15 20:19:29 +0200

    transactionsController

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/auth-slice';
+ import transactionsControllerReducer from './transactionsController/slice'
 import {
   persistStore,
   persistReducer,
@@ -21,6 +22,7 @@ const authPersistConfig = {
 export const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authReducer),
+    transactionsControllerReducer:transactionsControllerReducer,
     // auth: authReducer,
   },
   middleware: getDefaultMiddleware =>
