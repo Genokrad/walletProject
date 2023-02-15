2540485b867ee66d152958c47cbff717b0cced64
Dmytro Kharchenko <dmytro190589@gmail.com>
2023-02-15 16:08:48 +0200

    fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,10 @@ import LogoIcon from 'images/Header/logo-icon.svg'
 import Delimetr from 'images/Header/delimetr.svg'
 import ExitIcon from 'images/Header/exit-icon.svg'
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import { Button, Container, Exit, H2, HeaderLine, ImgDel, ImgWallet, Logo, Name, Wrapper } from './Header.styled';
 const Header = () => {
-    const user = useSelector(state=>state.auth.user.name);
+    // const user = useSelector(state=>state.auth.user.name);
     return (
         <Container>
             <HeaderLine>
@@ -16,7 +16,10 @@ const Header = () => {
                 </Logo>
                 <Wrapper>
                     <NavLink to="/???">
-                        <Name>{user}</Name>
+                        <Name>
+                            {/* {user} */}
+                            UserName
+                            </Name>
                     </NavLink>
                     <ImgDel src={Delimetr} alt="delimetr" />
                     <Button type="button"
