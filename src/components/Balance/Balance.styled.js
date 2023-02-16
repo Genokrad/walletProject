d83f4378cc5293574f878519227611c97d5a09e9
Genokrad <zeeper90@gmail.com>
2023-02-16 11:23:32 +0200

    balance

---
+++ src/components/Balance/Balance.styled.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 8px 40px;
+  justify-content: center;
+  width: 395px;
+  height: 80px;
+  background-color: var(--bcg-white-color);
+  border-radius: 30px;
+`;
+
+export const StyledPBalance = styled.div`
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 30px;
+  line-height: 45px;
+`;
+
+export const StyledPTite = styled.div`
+  font-family: 'Circe';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 18px;
+  text-transform: uppercase;
+  color: var(--balance-text-color);
+`;
+// export const CurrencyStyled = styled.table`
+//   padding: 5px;
+//   margin-top: 50px;
+//   margin-left: 40px;
+//   width: 280px;
+//   height: 174px;
+//   left: 20px;
+//   top: 144px;
+//   color: white;
+
+//   background: #4a56e2;
+//   border-radius: 30px;
+//   thead {
+//   }
+
+//   tbody {
+//   }
+// `;
