3ebac281c111cc151d8813746623a48ccedbd5b6
Dmytro Kharchenko <dmytro190589@gmail.com>
2023-02-17 19:13:28 +0200

    diagram

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -2,54 +2,44 @@ import React from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
 import './Chart.styled.js';
-import { Wrapper,TextAbs } from './Chart.styled.js';
-
+import { Wrapper, TextAbs } from './Chart.styled.js';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const Chart = () =>
-  // { colors, date, balance }
+export const Chart = ({ colors, date, balance }) => {
+  
+  const expenses = date.filter(item => item.type === 'EXPENSE');
 
-  {
-    //   const { categoriesSummary } = date;
-    //   console.log('categoriesSummary :>> ', categoriesSummary);
-    //   const expenses = categoriesSummary.filter(item => item.type === 'EXPENSE');
-
-    //   const labels = expenses.map(el => el.name);
-    //   let amount = expenses.map(el => el.total);
-    const balance = 1000;
-    const options = {
-      plugins: {
-        legend: {
-          display: false,
-        },
+  const labels = expenses.map(el => el.name);
+  let amount = expenses.map(el => el.total);
+  const colories = colors.map(el =>el.color)
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
       },
-    };
-    const data = {
-      labels: ['He', 'Hehehe'],
-
-      // labels,
-      datasets: [
-        {
-          data: [500, 500],
-
-          // amount,
-          backgroundColor: ['yellow', 'blue'],
-          //  colors
-          borderWidth: 0,
-        },
-      ],
-    };
-
-    return (
-      <Wrapper>
-        <TextAbs>{balance}</TextAbs>
-        <Doughnut
-          className="relative"
-          //    key={labels.name}
-          data={data}
-          options={options}
-        />
-      </Wrapper>
-    );
+    },
   };
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        data: amount,
+        backgroundColor: colories,
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  return (
+    <Wrapper>
+      <TextAbs>{balance}</TextAbs>
+      <Doughnut
+        className="relative"
+        //    key={labels.name}
+        data={data}
+        options={options}
+      />
+    </Wrapper>
+  );
+};
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,28 +1,28 @@
 import { Chart } from 'components/Chart/Chart';
 import { Stateless } from 'components/Stateless/Stateless';
-// import { useSelector } from 'react-redux';
-// import { selectStatSummury } from 'redux/statistics/selectorStatistics';
-// import { selectIsTotalBalance } from 'redux/finance/finance-selectors';
-// const colorExpenses = [
-//   { color: '#FED057' },
-//   { color: '#FFD8D0' },
-//   { color: '#FD9498' },
-//   { color: '#C5BAFF' },
-//   { color: '#6E78E8' },
-//   { color: '#4A56E2' },
-//   { color: '#81E1FF' },
-//   { color: '#24CCA7' },
-//   { color: '#00AD84' },
-// ];
+import { useSelector } from 'react-redux';
+import { selectStatSummury } from 'redux/statistics/selectorStatistics';
+import { selectIsTotalBalance } from 'redux/finance/finance-selectors';
+const colorExpenses = [
+  { color: '#FED057' },
+  { color: '#FFD8D0' },
+  { color: '#FD9498' },
+  { color: '#C5BAFF' },
+  { color: '#6E78E8' },
+  { color: '#4A56E2' },
+  { color: '#81E1FF' },
+  { color: '#24CCA7' },
+  { color: '#00AD84' },
+];
 export const Statistics = () => {
-  // const totalSummary = useSelector(selectStatSummury);
-  // const balance = useSelector(selectIsTotalBalance);
+  const totalSummary = useSelector(selectStatSummury);
+  const balance = useSelector(selectIsTotalBalance);
   return (
     <>
       <Chart
-        // colors={colorExpenses.color}
-        // date={totalSummary}
-        // balance={balance}
+        colors={colorExpenses}
+        date={totalSummary}
+        balance={balance}
       />
       <Stateless />
     </>
