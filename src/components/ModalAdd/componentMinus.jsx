d445868c75e6a9aa5db9af362bac787ebc337c9c
Genokrad <zeeper90@gmail.com>
2023-02-17 17:33:37 +0200

    remove

--- src/components/ModalAdd/componentMinus.jsx
+++ src/components/ModalAdd/componentMinus.jsx
@@ -16,57 +16,59 @@ import {
   DivBtn,
   BtnAdd,
   BtnCancel,
-  Data,
+  // Data,
 } from './StyledContent';
 const MinusComponent = () => {
   const [data, getData] = useState(new Date());
   const [type, getType] = useState('INCOME');
+  const [id, getId] = useState('');
   // const [category, getCategory] = useState('');
   const [comment, getComment] = useState('');
   const [amount, getAmount] = useState('');
   const getCategory = useSelector(getCat);
-  
-  const neededCat = getCategory.find(cat => cat.type === 'INCOME');
+
+  // const neededCat = getCategory.find(cat => cat.type === 'INCOME');
   // console.log(data);
   const dispatch = useDispatch();
   const handleChange = e => {
     if (e.target.name === 'sum') {
       getAmount(e.currentTarget.value);
-    
     } else if (e.target.name === 'data') {
       getData(new Date().toISOString());
       // getData(e.currentTarget.value);
-    
     } else if (e.target.name === 'comment') {
-     
       getComment(e.currentTarget.value);
+    } else if (e.target.name === 'select') {
+      const typeOfSelector = getCategory.find(
+        obj => obj.name === e.target.value
+      );
+      getType(typeOfSelector.type);
+      getId(typeOfSelector.id);
     }
   };
 
-  const fn = e=>{
-    console.log(e.target)
-   
-  }
-  // const handleSubmit = evt => {
-  //   evt.preventDefault();
-  //   const operation = {
-  //     transactionDate: data,
-  //     type: neededCat.type,
-  //     categoryId: neededCat.id,
-  //     comment: comment,
-  //     amount: amount,
-  //   };
-  //   dispatch(createTransaction(operation));
-  // };
+  console.log(type);
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    const operation = {
+      transactionDate: data,
+      type: type,
+      categoryId: id,
+      comment: comment,
+      amount: -amount,
+    };
+    dispatch(createTransaction(operation));
+  };
   return (
     <>
       <DivSetting className="SetingTransaction">
         <form>
           <Forms>
             <FormsInput placeholder="Select a category" />
-            <FormsSelect id="category" >
+            <FormsSelect id="category" onChange={handleChange} name="select">
               {getCategory.map(category => {
-                return <Option key={category.id} >{category.name}</Option>;
+                return <Option key={category.id}>{category.name}</Option>;
               })}
             </FormsSelect>
           </Forms>
@@ -90,7 +92,7 @@ const MinusComponent = () => {
         </form>
       </DivSetting>
       <DivBtn className="Btn">
-        <BtnAdd>ADD</BtnAdd>
+        <BtnAdd onClick={handleSubmit}>ADD</BtnAdd>
         <BtnCancel>CANCEL</BtnCancel>
       </DivBtn>
     </>
