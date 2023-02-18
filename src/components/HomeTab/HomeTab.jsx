4e29b75ce8c7c567965461a177c2f80572716955
SerhiiKutovyi <wagenrex@gmail.com>
2023-02-18 11:15:16 +0200

    Update HomeTab.jsx

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -24,101 +24,97 @@ import { useEffect } from 'react';
 
 const HomeTab = () => {
   const dispatch = useDispatch();
-  const data = useSelector(selectTransactions);
+  const records = useSelector(selectTransactions);
 
-  console.log('transactions', data);
+  console.log('transactions', records);
 
   useEffect(() => {
     dispatch(getAllTransactions());
   }, [dispatch]);
 
   return (
-    <div>
-      <Table>
-        <thead>
-          <TopTable>
-            <Data>Date</Data>
-            <Type>Type</Type>
-            <Category>Category</Category>
-            <Comment>Comment</Comment>
-            <Sum>Sum</Sum>
-            <th> </th>
-          </TopTable>
-        </thead>
-        <tbody>
-          {data.map(
-            ({
-              transactionDate,
-              balanceAfter,
-              type,
-              categoryId,
-              comment,
-              id,
-            }) => (
-              <>
-                <TableBody>
-                  <DataTd key={id}>{transactionDate}</DataTd>
-                  <TypeTd>{type}</TypeTd>
-                  <CategoryTd>{categoryId}</CategoryTd>
-                  <CommentTd>{comment}</CommentTd>
-                  <SumTd>{balanceAfter}</SumTd>
-                  <Action>
-                    <svg width="14" height="14">
-                      <use
-                        href={sprite + '#iconpencel'}
-                        width="14"
-                        height="14"
-                      ></use>
-                    </svg>
-                    <Btn>Delete</Btn>
-                  </Action>
-                </TableBody>
-              </>
-            )
-          )}
+    <Table>
+      <thead>
+        <TopTable>
+          <Data>Date</Data>
+          <Type>Type</Type>
+          <Category>Category</Category>
+          <Comment>Comment</Comment>
+          <Sum>Sum</Sum>
+          <th> </th>
+        </TopTable>
+      </thead>
 
-          {/* <TableBody>
-            <DataTd>04.01.19</DataTd>
-            <TypeTd>-</TypeTd>
-            <CategoryTd>Ouher</CategoryTd>
-            <CommentTd>Gift</CommentTd>
-            <SumTd>300</SumTd>
-            <Action>
-              <svg width="14" height="14">
-                <use href={sprite + '#iconpencel'} width="14" height="14"></use>
-              </svg>
-              <Btn>Delete</Btn>
-            </Action>
-          </TableBody>
-          <TableBody>
-            <DataTd>04.01.22</DataTd>
-            <TypeTd>+</TypeTd>
-            <CategoryTd>Gars</CategoryTd>
-            <CommentTd>Fuel</CommentTd>
-            <SumTd>1000</SumTd>
-            <Action>
-              <svg width="14" height="14">
-                <use href={sprite + '#iconpencel'} width="14" height="14"></use>
-              </svg>
-              <Btn>Delete</Btn>
-            </Action>
-          </TableBody>
-          <TableBody>
-            <DataTd>04.01.19</DataTd>
-            <TypeTd>-</TypeTd>
-            <CategoryTd>Ouher</CategoryTd>
-            <CommentTd>Gift</CommentTd>
-            <SumTd>300</SumTd>
-            <Action>
-              <svg width="14" height="14">
-                <use href={sprite + '#iconpencel'} width="14" height="14"></use>
-              </svg>
-              <Btn>Delete</Btn>
-            </Action>
-          </TableBody> */}
-        </tbody>
-      </Table>
-    </div>
+      {records.map(
+        ({ transactionDate, balanceAfter, type, categoryId, id, comment }) => (
+          <>
+            <tbody>
+              <TableBody key={id}>
+                <DataTd>{transactionDate}</DataTd>
+                <TypeTd>
+                  {(type === 'INCOME' && '+') || (type === 'EXPENSE' && '-')}
+                </TypeTd>
+                <CategoryTd>{categoryId}</CategoryTd>
+                <CommentTd>{comment}</CommentTd>
+                <SumTd>{balanceAfter}</SumTd>
+                <Action>
+                  <svg width="14" height="14">
+                    <use
+                      href={sprite + '#iconpencel'}
+                      width="14"
+                      height="14"
+                    ></use>
+                  </svg>
+                </Action>
+                <Btn>Delete</Btn>
+              </TableBody>
+            </tbody>
+          </>
+        )
+      )}
+    </Table>
   );
 };
 export default HomeTab;
+
+// {
+//   /* <TableBody>
+//           <DataTd>04.01.19</DataTd>
+//           <TypeTd>-</TypeTd>
+//           <CategoryTd>Ouher</CategoryTd>
+//           <CommentTd>Gift</CommentTd>
+//           <SumTd>300</SumTd>
+//           <Action>
+//             <svg width="14" height="14">
+//               <use href={sprite + '#iconpencel'} width="14" height="14"></use>
+//             </svg>
+//             <Btn>Delete</Btn>
+//           </Action>
+//         </TableBody>
+//         <TableBody>
+//           <DataTd>04.01.22</DataTd>
+//           <TypeTd>+</TypeTd>
+//           <CategoryTd>Gars</CategoryTd>
+//           <CommentTd>Fuel</CommentTd>
+//           <SumTd>1000</SumTd>
+//           <Action>
+//             <svg width="14" height="14">
+//               <use href={sprite + '#iconpencel'} width="14" height="14"></use>
+//             </svg>
+//             <Btn>Delete</Btn>
+//           </Action>
+//         </TableBody>
+//         <TableBody>
+//           <DataTd>04.01.19</DataTd>
+//           <TypeTd>-</TypeTd>
+//           <CategoryTd>Ouher</CategoryTd>
+//           <CommentTd>Gift</CommentTd>
+//           <SumTd>300</SumTd>
+//           <Action>
+//             <svg width="14" height="14">
+//               <use href={sprite + '#iconpencel'} width="14" height="14"></use>
+//             </svg>
+//             <Btn>Delete</Btn>
+//           </Action>
+//         </TableBody> */
+// }
