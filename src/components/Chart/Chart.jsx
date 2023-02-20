f4cfa659f1fcbb4240f186884098161767e8bd26
Dmytro Kharchenko <dmytro190589@gmail.com>
2023-02-20 15:24:38 +0200

    ChartFixed

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
-import './Chart.styled.js';
 import { Wrapper, TextAbs } from './Chart.styled.js';
 import { Loader } from 'components/Loader/Loader.jsx';
+import {handleColor } from 'redux/statistics/stat-color.js';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const Chart = ({ colors, date, balance }) => {
+export const Chart = ({ date, balance }) => {
   const expenses = date.filter(item => item.type === 'EXPENSE');
 
   const labels = expenses.map(el => el.name);
   let amount = expenses.map(el => Math.abs(el.total));
-  const color = index => colors.find((_, ind )=> ind === index);
+
+  const color = labels.map(item => handleColor(item));
+
   const options = {
     plugins: {
       legend: {
