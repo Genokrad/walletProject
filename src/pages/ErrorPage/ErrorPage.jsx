25b1a8fa93c6b6837a0e629301a4f7d37a138cef
Genokrad <zeeper90@gmail.com>
2023-02-20 22:05:35 +0200

    error

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import image from '../../images/error/enot.png';
+
+import { Container, Wraper, Number, Text, StyledNavLink } from './styled.error';
 
 export const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <Container>
+      <Wraper>
+        <div>
+          <Number>404!</Number>
+        </div>
+        <div>
+          <img src={image} alt="enot" />
+        </div>
+        <div>
+          <Text>Page not found</Text>
+        </div>
+        <div>
+          <StyledNavLink to={'/'}>Home</StyledNavLink>
+        </div>
+      </Wraper>
+    </Container>
+  );
 };
